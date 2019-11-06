9203f1693bedd77220aeef6df64d6f81f8721cab
Groznetscky Arseniy <senya.groznetscky@gmail.com>
2019-11-06 14:53:46 +0300

    Complemented lab2/7упр.py

--- lab2/7упр.py
+++ lab2/7упр.py
@@ -2,17 +2,18 @@ import turtle
 import math
 
 def paint_polygon(n,r): 
-    a=2*r*math.sin( math.radians(180 / n ))/l
+    a=2*r*math.sin( math.radians(180 / n ))/m
     k=(n-2)/n*180
     turtle.forward(a)  
-    for i in range ((n-1)//359):
-        turtle.left(180-k)
-        turtle.forward(a)
+    turtle.left(180-k)
 
 turtle.shape('turtle')               
 n = 360
-l = 50
+m = 10
 for r in range (1,5000):
     paint_polygon(n,r)
-
-    
+# m - the inverse pitch of the helix
+# r - the radius of the spiral, it increases evenly
+# n - the number of vertices of a polygon inscribed in a circle of radius r
+# k - the angle of rotation of a turtle
+                  
--- lab3/smile.py
+++ lab3/smile.py
@@ -1,2 +1,6 @@
 from graph import*
 
+penColor("yellow")
+brushColor("red")
+circle(200, 200, 70)
+
