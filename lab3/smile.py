9996961d56fd8fde8168e1b38ef7a1beb476b520
Groznetscky Arseniy <senya.groznetscky@gmail.com>
2019-11-06 17:02:52 +0300

    Finished lab3/smile.py

--- lab3/smile.py
+++ lab3/smile.py
@@ -1,6 +1,29 @@
 from graph import*
 
-penColor("yellow")
-brushColor("red")
+penColor("red")
+brushColor("yellow")
 circle(200, 200, 70)
 
+penColor("black")
+brushColor("red")
+circle(170, 180, 15)
+penColor("black")
+brushColor("black")
+circle(170, 180, 5)
+
+penColor("black")
+brushColor("red")
+circle(230, 180, 10)
+penColor("black")
+brushColor("black")
+circle(230, 180, 5)
+
+penColor("black")
+brushColor("black")
+polygon([(135,135), (200,175), (195,180), (130,140), (135,135)])
+
+penColor("black")
+brushColor("black")
+polygon([(270,150), (275,155), (210,176), (210,166), (270,150)])
+
+rectangle(170, 225, 230,240 )
