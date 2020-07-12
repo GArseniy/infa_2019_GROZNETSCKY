d8c48d28955d45c51144f0e3be8f307e85097755
GArseniy TOSHIBA <senya.groznetscky@gmail.com>
2020-07-13 00:39:34 +0500

    neural network

---
+++ neuralNetwork/digitizer.py
@@ -0,0 +1,52 @@
+import numpy
+import scipy.special
+import matplotlib.pyplot
+
+
+class neural_network:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.wih = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        self.lr = learningrate
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(inputs))
+
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+
+input_nodes = 784
+hiden_nodes = 100
+output_nodes = 10
+learning_rate = 0.3
+
+n = neural_network(input_nodes, hiden_nodes, output_nodes, learning_rate)
+
+with open("mnist_dataset/mnist_train_100.csv", 'r') as data:
+    training_data_list = data.readlines()
+
+for record in training_data_list:
+    all_values = record.split(',')
+    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    targets = numpy.zeros(output_nodes) + 0.01
+    targets[int(all_values[0])] = 0.99
+    n.train(inputs, targets)
